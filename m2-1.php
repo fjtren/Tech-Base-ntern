61f224dd3af272f3c6364f5303b099600668d2e6
fjtren <0303ner@gmail.com>
2023-09-25 17:34:14 +0900

    add: m5-1.php

---
+++ m2-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>mission_2-1</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="str" value="コメント">
+        <input type="submit" name="submit" value="送信">
+    </form>
+    <?php
+        $str=$_POST["str"];
+        echo $str."を受け付けました";
+    ?>
+</body>
+
+</html>
